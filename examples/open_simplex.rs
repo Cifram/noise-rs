1328398995670a1a8c9cfb5ea05e73606940d463
Aubrey Powell <herufeanor@gmail.com>
2021-08-15 15:10:35 -0700

    Fix open simplex example
    It now calls the core functions directly, and generated 2D, 3D and 4D
    images.

--- examples/open_simplex.rs
+++ examples/open_simplex.rs
@@ -2,18 +2,30 @@
 
 extern crate noise;
 
-use noise::{utils::*, OpenSimplex, Seedable};
+use noise::{
+    utils::*,
+    core::open_simplex::{open_simplex_2d, open_simplex_3d, open_simplex_4d},
+    permutationtable::PermutationTable
+};
 
 fn main() {
-    let open_simplex = OpenSimplex::default();
-
-    PlaneMapBuilder::new(open_simplex)
+    let hasher = PermutationTable::new(0);
+    PlaneMapBuilder::new_fn(open_simplex_2d, &hasher)
+        .set_size(1024, 1024)
+        .set_x_bounds(-5.0, 5.0)
+        .set_y_bounds(-5.0, 5.0)
         .build()
-        .write_to_file("open_simplex.png");
-
-    let open_simplex = open_simplex.set_seed(1);
-
-    PlaneMapBuilder::new(open_simplex)
+        .write_to_file("open_simplex 2d.png");
+    PlaneMapBuilder::new_fn(open_simplex_3d, &hasher)
+        .set_size(1024, 1024)
+        .set_x_bounds(-5.0, 5.0)
+        .set_y_bounds(-5.0, 5.0)
         .build()
-        .write_to_file("open_simplex_seed=1.png");
+        .write_to_file("open_simplex 3d.png");
+    PlaneMapBuilder::new_fn(open_simplex_4d, &hasher)
+        .set_size(1024, 1024)
+        .set_x_bounds(-5.0, 5.0)
+        .set_y_bounds(-5.0, 5.0)
+        .build()
+        .write_to_file("open_simplex 4d.png");
 }
