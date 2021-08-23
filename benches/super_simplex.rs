8a0826ff85896e80495728038cf56cb704ad75ae
Aubrey Powell <herufeanor@gmail.com>
2021-08-23 00:38:24 -0700

    Update super simplex to use vectors
    The super simplex core functions now take a Vector2/3 as it's input,
    instead of taking an array. But the 2D function is converted from using
    vectors for all it's internal logic, because breaking it out and doing
    the logic directly on the component scalars got a significant
    improvement on the benchmarks. This did not help for the 3D function, so
    that was left using the vectors.

--- benches/super_simplex.rs
+++ benches/super_simplex.rs
@@ -5,6 +5,7 @@ extern crate noise;
 use criterion::{black_box, Criterion};
 use noise::{
     core::super_simplex::{super_simplex_2d, super_simplex_3d},
+    math::vectors::{Vector2, Vector3},
     permutationtable::PermutationTable,
 };
 
@@ -23,14 +24,14 @@ criterion_main!(super_simplex, super_simplex_64x64);
 fn bench_super_simplex2(c: &mut Criterion) {
     let hasher = PermutationTable::new(0);
     c.bench_function("super simplex 2d", |b| {
-        b.iter(|| black_box(super_simplex_2d([42.0_f64, 37.0], &hasher)))
+        b.iter(|| black_box(super_simplex_2d(Vector2::new(42.0_f64, 37.0), &hasher)))
     });
 }
 
 fn bench_super_simplex3(c: &mut Criterion) {
     let hasher = PermutationTable::new(0);
     c.bench_function("super simplex 3d", |b| {
-        b.iter(|| black_box(super_simplex_3d([42.0_f64, 37.0, 26.0], &hasher)))
+        b.iter(|| black_box(super_simplex_3d(Vector3::new(42.0_f64, 37.0, 26.0), &hasher)))
     });
 }
 
@@ -40,7 +41,7 @@ fn bench_super_simplex2_64x64(c: &mut Criterion) {
         b.iter(|| {
             for y in 0i8..64 {
                 for x in 0i8..64 {
-                    black_box(super_simplex_2d([x as f64, y as f64], &hasher));
+                    black_box(super_simplex_2d(Vector2::new(x as f64, y as f64), &hasher));
                 }
             }
         })
@@ -53,7 +54,7 @@ fn bench_super_simplex3_64x64(c: &mut Criterion) {
         b.iter(|| {
             for y in 0i8..64 {
                 for x in 0i8..64 {
-                    black_box(super_simplex_3d([x as f64, y as f64, x as f64], &hasher));
+                    black_box(super_simplex_3d(Vector3::new(x as f64, y as f64, x as f64), &hasher));
                 }
             }
         })
--- src/core/super_simplex.rs
+++ src/core/super_simplex.rs
@@ -23,7 +23,7 @@ const NORM_CONSTANT_3D: f64 = 1.0 / 0.086_766_400_165_536_9;
 //                         \   |
 //                          ( 1,  2)
 #[rustfmt::skip]
-const LATTICE_LOOKUP_2D: [([i8; 2], [f64; 2]); 4 * 8] =
+const LATTICE_LOOKUP_2D: [([isize; 2], [f64; 2]); 4 * 8] =
     [([0, 0], [0_f64, 0_f64]),
      ([1, 1], [-0.577_350_269_189_626_f64, -0.577_350_269_189_626_f64]),
      ([-1, 0], [0.788_675_134_594_813_f64, -0.211_324_865_405_187_f64]),
@@ -65,7 +65,7 @@ const LATTICE_LOOKUP_2D: [([i8; 2], [f64; 2]); 4 * 8] =
      ([1, 2], [-0.366_025_403_784_439_04_f64, -1.366_025_403_784_439_f64])];
 
 #[rustfmt::skip]
-const LATTICE_LOOKUP_3D: [[i8; 3]; 4 * 16] =
+const LATTICE_LOOKUP_3D: [[isize; 3]; 4 * 16] =
     [[0, 0, 0],[1, 0, 0],[0, 1, 0],[0, 0, 1],
      [1, 1, 1],[1, 0, 0],[0, 1, 0],[0, 0, 1],
      [0, 0, 0],[0, 1, 1],[0, 1, 0],[0, 0, 1],
@@ -83,113 +83,102 @@ const LATTICE_LOOKUP_3D: [[i8; 3]; 4 * 16] =
      [0, 0, 0],[0, 1, 1],[1, 0, 1],[1, 1, 0],
      [1, 1, 1],[0, 1, 1],[1, 0, 1],[1, 1, 0]];
 
-pub fn super_simplex_2d(point: [f64; 2], hasher: &PermutationTable) -> f64 {
-    let point = Vector2::from(point);
+pub fn super_simplex_2d(point: Vector2<f64>, hasher: &PermutationTable) -> f64 {
+    // Using the vector struct internally causes a significant performance
+    // regression, so break out it's components here and use those.
+    let (x, y) = point.into();
 
     // Transform point from real space to simplex space
-    let to_simplex_offset = point.sum() * TO_SIMPLEX_CONSTANT_2D;
-    let simplex_point = point.map(|v| v + to_simplex_offset);
+    let to_simplex_offset = (x + y) * TO_SIMPLEX_CONSTANT_2D;
+    let simplex_point_x = x + to_simplex_offset;
+    let simplex_point_y = y + to_simplex_offset;
 
     // Get base point of simplex and barycentric coordinates in simplex space
-    let simplex_base_point = simplex_point.floor();
-    let simplex_base_point_i = simplex_base_point.numcast::<isize>().unwrap();
-    let simplex_rel_coords = simplex_point - simplex_base_point;
+    let simplex_floor_x = simplex_point_x.floor();
+    let simplex_floor_y = simplex_point_y.floor();
+    let simplex_cell_x = simplex_floor_x as isize;
+    let simplex_cell_y = simplex_floor_y as isize;
+    let simplex_rel_x = simplex_point_x - simplex_floor_x;
+    let simplex_rel_y = simplex_point_y - simplex_floor_y;
 
     // Create index to lookup table from barycentric coordinates
-    let region_sum = simplex_rel_coords.sum().floor();
-    let index = ((region_sum >= 1.0) as usize) << 2
-        | ((simplex_rel_coords.x - simplex_rel_coords.y * 0.5 + 1.0 - region_sum * 0.5 >= 1.0)
-            as usize)
-            << 3
-        | ((simplex_rel_coords.y - simplex_rel_coords.x * 0.5 + 1.0 - region_sum * 0.5 >= 1.0)
-            as usize)
-            << 4;
+    let region_sum = (simplex_rel_x + simplex_rel_y).floor();
+    let index =
+        ((region_sum >= 1.0) as usize) << 2 |
+        ((simplex_rel_x - simplex_rel_y * 0.5 + 1.0 - region_sum * 0.5 >= 1.0) as usize) << 3 |
+        ((simplex_rel_y - simplex_rel_x * 0.5 + 1.0 - region_sum * 0.5 >= 1.0) as usize) << 4;
 
     // Transform barycentric coordinates to real space
-    let to_real_offset = simplex_rel_coords.sum() * TO_REAL_CONSTANT_2D;
-    let real_rel_coords = simplex_rel_coords.map(|v| v + to_real_offset);
+    let to_real_offset = (simplex_rel_x + simplex_rel_y) * TO_REAL_CONSTANT_2D;
+    let real_rel_x = simplex_rel_x + to_real_offset;
+    let real_rel_y = simplex_rel_y + to_real_offset;
 
     let mut value = 0.0;
 
     for lattice_lookup in &LATTICE_LOOKUP_2D[index..index + 4] {
-        let dpos = real_rel_coords + Vector2::from(lattice_lookup.1).numcast().unwrap();
-        let attn = (2.0 / 3.0) - dpos.magnitude_squared();
+        let [latticex, latticey] = lattice_lookup.1;
+        let dposx = real_rel_x + latticex;
+        let dposy = real_rel_y + latticey;
+        let attn = (2.0 / 3.0) - (dposx*dposx + dposy*dposy);
         if attn > 0.0 {
-            let lattice_point =
-                simplex_base_point_i + Vector2::from(lattice_lookup.0).numcast().unwrap();
-            let gradient = Vector2::from(gradient::grad2(hasher.hash_2d(lattice_point.into())));
-            value += attn.powi(4) * gradient.dot(dpos);
+            let lattice_point_x = lattice_lookup.0[0] + simplex_cell_x;
+            let lattice_point_y = lattice_lookup.0[1] + simplex_cell_y;
+            let [gradx, grady] = gradient::grad2(hasher.hash_2d([lattice_point_x, lattice_point_y]));
+            value += attn.powi(4) * (gradx*dposx + grady*dposy);
         }
     }
 
     value * NORM_CONSTANT_2D
 }
 
-pub fn super_simplex_3d(point: [f64; 3], hasher: &PermutationTable) -> f64 {
-    let point = Vector3::from(point);
-
+pub fn super_simplex_3d(point: Vector3<f64>, hasher: &PermutationTable) -> f64 {
     // Transform point from real space to simplex space
     let to_simplex_offset = point.sum() * TO_SIMPLEX_CONSTANT_3D;
-    let simplex_point = point.map(|v| -(v + to_simplex_offset));
-    let second_simplex_point = simplex_point.map(|v| v + 512.5);
+    let simplex_point_1 = point.map(|v| -(v + to_simplex_offset));
+    let second_simplex_point = simplex_point_1.map(|v| v + 512.5);
 
     // Get base point of simplex and barycentric coordinates in simplex space
-    let simplex_base_point = simplex_point.floor();
-    let simplex_base_point_i = simplex_base_point.numcast::<isize>().unwrap();
-    let simplex_rel_coords = simplex_point - simplex_base_point;
-    let second_simplex_base_point = second_simplex_point.floor();
-    let second_simplex_base_point_i = second_simplex_base_point.numcast::<isize>().unwrap();
-    let second_simplex_rel_coords = second_simplex_point - second_simplex_base_point;
+    let simplex_floor1 = simplex_point_1.floor();
+    let simplex_cell1 = simplex_floor1.numcast::<isize>().unwrap();
+    let simplex_rel1 = simplex_point_1 - simplex_floor1;
+    let simplex_floor2 = second_simplex_point.floor();
+    let simplex_cell2 = simplex_floor2.numcast::<isize>().unwrap();
+    let simplex_rel2 = second_simplex_point - simplex_floor2;
 
     // Create indices to lookup table from barycentric coordinates
-    let index = ((simplex_rel_coords.x + simplex_rel_coords.y + simplex_rel_coords.z >= 1.5)
-        as usize)
-        << 2
-        | ((-simplex_rel_coords.x + simplex_rel_coords.y + simplex_rel_coords.z >= 0.5) as usize)
-            << 3
-        | ((simplex_rel_coords.x - simplex_rel_coords.y + simplex_rel_coords.z >= 0.5) as usize)
-            << 4
-        | ((simplex_rel_coords.x + simplex_rel_coords.y - simplex_rel_coords.z >= 0.5) as usize)
-            << 5;
-    let second_index = ((second_simplex_rel_coords.x
-        + second_simplex_rel_coords.y
-        + second_simplex_rel_coords.z
-        >= 1.5) as usize)
-        << 2
-        | ((-second_simplex_rel_coords.x
-            + second_simplex_rel_coords.y
-            + second_simplex_rel_coords.z
-            >= 0.5) as usize)
-            << 3
-        | ((second_simplex_rel_coords.x - second_simplex_rel_coords.y + second_simplex_rel_coords.z
-            >= 0.5) as usize)
-            << 4
-        | ((second_simplex_rel_coords.x + second_simplex_rel_coords.y - second_simplex_rel_coords.z
-            >= 0.5) as usize)
-            << 5;
+    let index1 =
+        ((simplex_rel1.x + simplex_rel1.y + simplex_rel1.z >= 1.5) as usize) << 2 |
+        ((-simplex_rel1.x + simplex_rel1.y + simplex_rel1.z >= 0.5) as usize) << 3 |
+        ((simplex_rel1.x - simplex_rel1.y + simplex_rel1.z >= 0.5) as usize) << 4 |
+        ((simplex_rel1.x + simplex_rel1.y - simplex_rel1.z >= 0.5) as usize) << 5;
+    let index2 =
+        ((simplex_rel2.x + simplex_rel2.y + simplex_rel2.z >= 1.5) as usize) << 2 |
+        ((-simplex_rel2.x + simplex_rel2.y + simplex_rel2.z >= 0.5) as usize) << 3 |
+        ((simplex_rel2.x - simplex_rel2.y + simplex_rel2.z >= 0.5) as usize) << 4 |
+        ((simplex_rel2.x + simplex_rel2.y - simplex_rel2.z >= 0.5) as usize) << 5;
 
     let mut value = 0.0;
 
     // Sum contributions from first lattice
-    for &lattice_lookup in &LATTICE_LOOKUP_3D[index..index + 4] {
-        let dpos = simplex_rel_coords - Vector3::from(lattice_lookup).numcast().unwrap();
+    for &lattice_lookup in &LATTICE_LOOKUP_3D[index1..index1 + 4] {
+        let lattice = Vector3::from(lattice_lookup);
+        let dpos = simplex_rel1 - lattice.numcast().unwrap();
         let attn = 0.75 - dpos.magnitude_squared();
         if attn > 0.0 {
-            let lattice_point =
-                simplex_base_point_i + Vector3::from(lattice_lookup).numcast().unwrap();
-            let gradient = Vector3::from(gradient::grad3(hasher.hash_3d(lattice_point.into_array())));
+            let lattice_point = simplex_cell1 + lattice;
+            let gradient = Vector3::from(gradient::grad3(hasher.hash_3d(lattice_point.into())));
             value += attn.powi(4) * gradient.dot(dpos);
         }
     }
 
     // Sum contributions from second lattice
-    for &lattice_lookup in &LATTICE_LOOKUP_3D[second_index..second_index + 4] {
-        let dpos = second_simplex_rel_coords - Vector3::from(lattice_lookup).numcast().unwrap();
+    for &lattice_lookup in &LATTICE_LOOKUP_3D[index2..index2 + 4] {
+        let lattice = Vector3::from(lattice_lookup);
+        let dpos = simplex_rel2 - lattice.numcast().unwrap();
         let attn = 0.75 - dpos.magnitude_squared();
         if attn > 0.0 {
-            let lattice_point =
-                second_simplex_base_point_i + Vector3::from(lattice_lookup).numcast().unwrap();
-            let gradient = Vector3::from(gradient::grad3(hasher.hash_3d(lattice_point.into_array())));
+            let lattice_point = simplex_cell2 + lattice;
+            let gradient = Vector3::from(gradient::grad3(hasher.hash_3d(lattice_point.into())));
             value += attn.powi(4) * gradient.dot(dpos);
         }
     }
